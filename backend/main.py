0ca4241f636e77e060f15cba0c069cedef2e9c2c
STRANGEi7 <159226586+STRANGEi7@users.noreply.github.com>
2025-05-09 00:50:32 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -47,7 +47,7 @@ async def predict(file: UploadFile = File(...)):
     
     return JSONResponse(content={"disease": disease_name, "suggestion": suggestion})
 
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
 
-    if __name__ == "__main__":
-         uvicorn.run("main:app", host="0.0.0.0", port=8000)
 
